4c674c37037b589494d30c8e0cb6cfc8837ed734
Camila <mendemmcamila@gmail.com>
2024-03-15 19:22:28 -0300

    feat: install and configure SVG libraries

---
+++ src/assets/index.ts
@@ -0,0 +1,29 @@
+import ArrowLeft from "./arrowLeft.svg";
+import Barbell from "./barbell.svg";
+import CaretRight from "./caretRight.svg";
+import Clock from "./clock.svg";
+import ClockCounterClockwise from "./clockCounterClockwise.svg";
+import ClockCounterClockwiseBold from "./clockCounterClockwiseBold.svg";
+import House from "./house.svg";
+import HouseBold from "./houseBold.svg";
+import Person from "./person.svg";
+import Repeat from "./repeat.svg";
+import SignOut from "./signOut.svg";
+import UserCircle from "./userCircle.svg";
+import UserCircleBold from "./userCircleBold.svg";
+
+export {
+  ArrowLeft,
+  Barbell,
+  CaretRight,
+  Clock,
+  ClockCounterClockwise,
+  ClockCounterClockwiseBold,
+  House,
+  HouseBold,
+  Person,
+  Repeat,
+  SignOut,
+  UserCircle,
+  UserCircleBold,
+};
