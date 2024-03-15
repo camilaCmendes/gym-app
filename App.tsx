a2e01cc014e9762933a022c802913d7c5caec47d
Camila <mendemmcamila@gmail.com>
2024-03-15 20:00:02 -0300

    feat: install NativeBase library

--- App.tsx
+++ App.tsx
@@ -4,28 +4,23 @@ import {
   useFonts,
 } from "@expo-google-fonts/roboto";
 import { Text, View, StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import { Loading } from "@components/loading";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#202024",
-      }}
-    >
-      <StatusBar
-        barStyle={"light-content"}
-        backgroundColor={"transparent"}
-        translucent
-      />
-      {fontsLoaded ? (
-        <Text style={{ fontFamily: "Roboto_700Bold" }}>Open up App.tsx</Text>
-      ) : (
-        <View />
-      )}
-    </View>
+    <NativeBaseProvider>
+      <SafeAreaView style={{ flex: 1, padding: 24 }}>
+        <StatusBar
+          barStyle={"light-content"}
+          backgroundColor={"transparent"}
+          translucent
+        />
+        <Loading />
+        {fontsLoaded ? <View /> : <Loading />}
+      </SafeAreaView>
+    </NativeBaseProvider>
   );
 }
