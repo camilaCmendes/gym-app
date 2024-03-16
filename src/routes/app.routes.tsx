60f15f3f10d6e26d15747a54a5ccacefd01d16b8
Camila <mendemmcamila@gmail.com>
2024-03-16 17:39:39 -0300

    feat: customize bottom tabs

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -6,6 +6,9 @@ import { Exercise } from "@screens/exercise";
 import { History } from "@screens/history";
 import { Home } from "@screens/home";
 import { Profile } from "@screens/profile";
+import { House, ClockCounterClockwise, UserCircle } from "@assets/index";
+import { useTheme } from "native-base";
+import { Platform } from "react-native";
 
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
@@ -19,12 +22,64 @@ type AppRoutes = {
 export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
 
 export function AppRoutes() {
+  const theme = useTheme();
+  const iconSize = theme.sizes[6];
+
+  const activeColor = theme.colors.green[500];
+  const inativeColor = theme.colors.gray[200];
+  const tabBackgroundColor = theme.colors.gray[600];
   return (
-    <Navigator>
-      <Screen name="home" children={Home} />
-      <Screen name="history" children={History} />
-      <Screen name="profile" children={Profile} />
-      <Screen name="exercise" children={Exercise} />
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: activeColor,
+        tabBarInactiveTintColor: inativeColor,
+        tabBarStyle: {
+          backgroundColor: tabBackgroundColor,
+          borderTopWidth: 0,
+          height: Platform.OS === "android" ? "auto" : 96,
+          paddingBottom: theme.sizes[10],
+          paddingTop: theme.sizes[6],
+        },
+      }}
+    >
+      <Screen
+        name="home"
+        children={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <House width={iconSize} height={iconSize} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="history"
+        children={History}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <ClockCounterClockwise
+              width={iconSize}
+              height={iconSize}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Screen
+        name="profile"
+        children={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <UserCircle width={iconSize} height={iconSize} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="exercise"
+        children={Exercise}
+        options={{ tabBarButton: () => null }}
+      />
     </Navigator>
   );
 }
