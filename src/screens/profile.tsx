99d7c79773c31aa14c4dbc8b0ea84d269f84a06f
Camila <mendemmcamila@gmail.com>
2024-03-21 21:59:18 -0300

    feat: integrate library for changing user profile image

--- src/screens/profile.tsx
+++ src/screens/profile.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { TouchableOpacity } from "react-native";
+import { Alert, TouchableOpacity } from "react-native";
 import {
   Center,
   ScrollView,
@@ -7,6 +7,7 @@ import {
   Skeleton,
   Text,
   Heading,
+  useToast,
 } from "native-base";
 
 import { ScreenHeader } from "@components/screenHeader";
@@ -14,10 +15,60 @@ import { UserPhoto } from "@components/userPhoto";
 import { Input } from "@components/input";
 import { Button } from "@components/button";
 
+import * as FileSystem from "expo-file-system";
+import * as ImagePicker from "expo-image-picker";
+
 const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const toast = useToast();
+
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState(
+    "https://github.com/camilaCmendes.png"
+  );
+
+  async function handleUserPhotoSelected() {
+    setPhotoIsLoading(true);
+
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(
+          photoSelected.assets[0].uri
+        );
+
+        if (
+          "size" in photoInfo &&
+          photoInfo.size &&
+          photoInfo.size / 1024 / 1024 > 5
+        ) {
+          console.log("entrou");
+          return toast.show({
+            title: "Essa imagem é muito grande. Escolha uma de até 5MB.",
+            placement: "top",
+            bgColor: "red.500",
+          });
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  }
 
   return (
     <VStack flex={1}>
@@ -35,12 +86,12 @@ export function Profile() {
             />
           ) : (
             <UserPhoto
-              source={{ uri: "https://github.com/camilaCmendes.png" }}
+              source={{ uri: userPhoto }}
               alt="Foto do usuário"
               size={PHOTO_SIZE}
             />
           )}
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelected}>
             <Text
               color="green.500"
               fontWeight="bold"
