39925e46acb595b620ae10d1846158d0e76a50bf
Camila <mendemmcamila@gmail.com>
2024-03-27 15:04:18 -0300

    feat: JWT Authentication - Attaching token to requests

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -6,6 +6,7 @@ import {
   storageUserRemove,
   storageUserSave,
 } from "@storage/storageUser";
+import { storageAuthTokenSave } from "@storage/storageAuthToken";
 
 export type AuthContextDataProps = {
   user: UserDTO;
@@ -27,13 +28,28 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] =
     useState(true);
 
+  const storageUserAndToken = async (userData: UserDTO, token: string) => {
+    try {
+      setIsLoadingUserStorageData(true);
+
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+      await storageUserSave(userData);
+      await storageAuthTokenSave(token);
+      setUser(userData);
+    } catch (error) {
+      throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
+    }
+  };
+
   const singIn = async (email: string, password: string) => {
     try {
       const { data } = await api.post("/sessions", { email, password });
 
       if (data.user && data.token) {
-        setUser(data.user);
-        storageUserSave(data.user);
+        storageUserAndToken(data.user, data.token);
       }
     } catch (error) {
       throw error;
--- src/storage/storageAuthToken.ts
+++ src/storage/storageAuthToken.ts
@@ -2,6 +2,6 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { AUTH_TOKEN_STORAGE } from "@storage/storageConfig";
 
-export const storageAuthTokenSabe = async (token: string) => {
+export const storageAuthTokenSave = async (token: string) => {
   await AsyncStorage.setItem(AUTH_TOKEN_STORAGE, token);
 };
